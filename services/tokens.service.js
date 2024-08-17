7c209d8539a570497763c09fdff2cdf898064580
mdhossain07 <mrabir8097@gmail.com>
2024-08-17 09:50:34 +0600

    expiration date converts into seconds using dayjs

--- services/tokens.service.js
+++ services/tokens.service.js
@@ -1,4 +1,7 @@
 const jwt = require("jsonwebtoken");
+const dayjs = require("dayjs");
+const duration = require("dayjs/plugin/duration");
+dayjs.extend(duration);
 
 class TokenService {
   generateToken = (payload, secret, expiration) => {
@@ -12,6 +15,13 @@ class TokenService {
   };
 
   generateAuthTokens = async (payload) => {
+    const accessTokenExpiration = dayjs
+      .duration(process.env.ACCESS_TOKEN_EXPIRATION, "minutes")
+      .asSeconds();
+
+    const refreshTokenExpiration = dayjs
+      .duration(process.env.REFRESH_TOKEN_EXPIRATION, "days")
+      .asSeconds();
     const accessToken = this.generateToken(
       payload,
       process.env.ACCESS_TOKEN_SECRET,
@@ -24,8 +34,8 @@ class TokenService {
     );
 
     return {
-      access: { accessToken },
-      refresh: { refreshToken },
+      access: { accessToken, expiresIn: accessTokenExpiration },
+      refresh: { refreshToken, expiresIn: refreshTokenExpiration },
     };
   };
 }
